f9b4dbf8570e325bcab0e69e94b7dd3a18051caa
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-11-16 17:43:26 +0100

    add layout

---
+++ src/Layout.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
