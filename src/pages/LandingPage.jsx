702f6c2af11555ce784bc75742b2df2abe8454bb
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-11-16 17:44:43 +0100

    add landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,23 @@
+import Hero from "../assets/pexels-oliver-sjöström-1078981.jpg";
+import { BaseButton } from "../components/ButtonComponent";
+import { Link } from "react-router-dom";
+
+const LandingPage = () => {
+  return (
+    <div
+      className=" py-56 px-32 flex justify-center md:h-screen bg-cover bg-center text-center flex-col"
+      style={{ backgroundImage: `url(${Hero})` }}
+    >
+      <h1 className="text-9xl text-white">
+        Book your next vacation at Holidaze
+      </h1>
+      <div>
+        <Link to="/SignIn">
+          <BaseButton>Sign in</BaseButton>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
