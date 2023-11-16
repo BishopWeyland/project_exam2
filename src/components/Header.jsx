75249f5b477cab498720778e896c6a4089068467
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-11-16 12:47:43 +0100

    add new footer and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header/index";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
