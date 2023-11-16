fd0bf204a32c17c94acff8fb7c70181f96fea546
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-11-16 17:44:00 +0100

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="p-3 flex justify-center items-center">
+      <span> Holidaze, 2023 ©</span>
+    </footer>
+  );
+};
+
+export default Footer;
