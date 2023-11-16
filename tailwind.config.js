a793d3caadc7a48503d30ab0eae4bd374c2395c7
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-11-16 17:42:24 +0100

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-white": "#F6F6F6",
+        "brand-green": "#0E9072",
+        "brand-grey": "#464646",
+        "brand-red": "#900E0E",
+      },
+    },
   },
   plugins: [],
-}
-
+};
