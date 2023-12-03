be8a8eece80516046dbd1b8fc74e34519d9319ec
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-12-03 13:31:23 +0100

    add avatar edit to profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,51 @@
-const Profile = () => {
-  return <h1 className="text-3xl font-bold underline">Profile</h1>;
+import React, { useState } from "react";
+import { useUser } from "../context/UserContext";
+import ProfileEditComponent from "../components/ProfileEditComponent";
+import DefaultAvatar from "../assets/1891016_user_male_avatar_account_profile_icon.png";
+import { BaseButton } from "../components/ButtonComponent";
+import { Link } from "react-router-dom";
+
+const ProfilePage = () => {
+  const { userProfile } = useUser();
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleCloseEdit = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div>
+      <div>
+        {userProfile && (
+          <>
+            <div className="flex items-center justify-center">
+              <img
+                className="rounded-full mr-10 h-24"
+                src={userProfile.avatar || DefaultAvatar}
+                alt="Profile Avatar"
+                onClick={handleEditClick}
+              />
+              <span>{userProfile.name}</span>
+            </div>
+          </>
+        )}
+
+        {isEditing && <ProfileEditComponent onClose={handleCloseEdit} />}
+      </div>
+
+      {userProfile && userProfile.venueManager && (
+        <div className="flex justify-center">
+          <Link to="/CreateVenue">
+            <BaseButton>Create venue</BaseButton>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
-export default Profile;
+export default ProfilePage;
