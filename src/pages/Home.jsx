4542bcd52dded31fdb66b2120246187f05aeb33e
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-12-03 19:26:51 +0100

    add styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,14 @@ const Home = () => {
   return (
     <div>
       <div
-        className="relative py-56 px-32 flex bg-cover bg-center text-left flex-col rounded-md"
+        className="relative py-32 px-4 md:px-8 lg:px-16 flex bg-cover bg-center text-left flex-col rounded-md"
         style={{ backgroundImage: `url(${Hero})` }}
       >
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-60 rounded-md"></div>
-        <h1 className="text-9xl text-white relative z-10">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-white relative z-10">
           Unwind in a Dreamy Seaside Haven
         </h1>
-        <span className="mt-3 text-2xl text-white relative z-10">
+        <span className="mt-3 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl text-white relative z-10">
           Book venues today at Holidaze
         </span>
       </div>
