1c99df12b6de269e78f1b29a751585e143909b5b
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-12-03 13:24:31 +0100

    add user to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,29 @@
-import Logo from "../assets/logo.png";
+import React from "react";
 import { NavLink } from "react-router-dom";
-
+import { useUser } from "../context/UserContext";
+import Logo from "../assets/logo.png";
+import DefaultAvatar from "../assets/1891016_user_male_avatar_account_profile_icon.png";
 const Header = () => {
+  const { userProfile } = useUser();
+
   return (
     <header className="z-10">
-      <nav className="flex justify-between mb-5">
+      <nav className="flex justify-between items-center mb-5">
         <NavLink to="/">
           <img className="h-20" src={Logo} alt="Logo" />
         </NavLink>
 
-        <NavLink to="/profile">
-          <span>Hello</span>
+        <NavLink to="/profile" className="flex items-center">
+          {userProfile && (
+            <>
+              <img
+                className="h-10 w-10 rounded-full object-cover"
+                src={userProfile.avatar || DefaultAvatar}
+                alt="User Avatar"
+              />
+              <span className="ml-2">{userProfile.name}</span>
+            </>
+          )}
         </NavLink>
       </nav>
     </header>
