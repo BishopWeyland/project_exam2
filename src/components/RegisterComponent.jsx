41fd65c3509d417cc920a4780c23712676152339
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-12-03 23:45:20 +0100

    style

--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import api from "../api/Api";
 import { BaseButton } from "./ButtonComponent";
+import { useNavigate } from "react-router-dom";
 
 const RegisterComponent = () => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -53,6 +55,7 @@ const RegisterComponent = () => {
       try {
         const response = await api.registerUser(formData);
         console.log("Registration response:", response);
+        navigate("/signin");
       } catch (error) {
         console.error("Registration error:", error.message);
       }
@@ -60,70 +63,73 @@ const RegisterComponent = () => {
   };
 
   return (
-    <div className="w-96 p-12 background drop-shadow-md">
-      <h1 className="text-3xl">Register user</h1>
-      <form className="flex flex-col">
-        <label className="flex flex-col">
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-          />
-        </label>
+    <div className="flex justify-center">
+      {" "}
+      <div className="w-96 p-12 background shadow-md">
+        <h1 className="text-3xl">Register user</h1>
+        <form className="flex flex-col">
+          <label className="flex flex-col">
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </label>
 
-        <label className="flex flex-col">
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-        </label>
+          <label className="flex flex-col">
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </label>
 
-        <label className="flex flex-col">
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </label>
+          <label className="flex flex-col">
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </label>
 
-        <label className="flex flex-col">
-          Avatar URL:
-          <input
-            type="text"
-            name="avatar"
-            value={formData.avatar}
-            onChange={handleInputChange}
-          />
-        </label>
+          <label className="flex flex-col">
+            Avatar URL:
+            <input
+              type="text"
+              name="avatar"
+              value={formData.avatar}
+              onChange={handleInputChange}
+            />
+          </label>
 
-        <label>
-          <input
-            type="checkbox"
-            name="venueManager"
-            checked={formData.venueManager}
-            onChange={handleInputChange}
-          />
-          Venue Manager:
-        </label>
+          <label>
+            <input
+              type="checkbox"
+              name="venueManager"
+              checked={formData.venueManager}
+              onChange={handleInputChange}
+            />
+            Venue Manager:
+          </label>
 
-        {errorMessages.length > 0 && (
-          <div style={{ color: "red" }}>
-            {errorMessages.map((error, index) => (
-              <p key={index}>{error}</p>
-            ))}
-          </div>
-        )}
-        <button type="button" onClick={handleRegister}>
-          Register
-        </button>
-      </form>
+          {errorMessages.length > 0 && (
+            <div style={{ color: "red" }}>
+              {errorMessages.map((error, index) => (
+                <p key={index}>{error}</p>
+              ))}
+            </div>
+          )}
+          <BaseButton type="button" onClick={handleRegister}>
+            Register
+          </BaseButton>
+        </form>
+      </div>
     </div>
   );
 };
